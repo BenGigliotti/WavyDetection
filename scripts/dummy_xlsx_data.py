a2f3c797988584059433918736e2e7dc347634c7
Ariel <arielmich64@gmail.com>
2025-11-08 14:05:11 -0500

    update dummy static data generator to support custom sampling rates

--- scripts/dummy_xlsx_data.py
+++ scripts/dummy_xlsx_data.py
@@ -3,27 +3,28 @@ import numpy as np
 from datetime import datetime, timedelta
 import random
 
-def generate_excel_data():    
+def generate_excel_data():
+    sampling_rate = int(input("Enter the sampling rate (number of samples per second): "))
     num_rows = int(input("Enter the number of rows/samples: "))
     mean_od = float(input("Enter the mean OD value: "))
     od_deviation = float(input("Enter the average deviation from mean OD: "))
     
     # generate timestamps
     start_time = datetime.now()
-    timestamps = [start_time + timedelta(seconds=i) for i in range(num_rows)]
+    timestamps = [start_time + timedelta(seconds=i/sampling_rate) for i in range(num_rows)]
     
     # generate speed
     speed_data = []
     i = 0
     while i < num_rows:
-        # random length of non-zero speed period (10-50 samples)
-        active_period = random.randint(10, 50)
-        # random length of zero speed period (5-20 samples)
-        zero_period = random.randint(5, 20)
+        # random length of non-zero speed period
+        active_period = random.randint(30*sampling_rate, 60*sampling_rate)
+        # random length of zero speed period
+        zero_period = random.randint(5*sampling_rate, 20*sampling_rate)
         
-        # add non-zero speeds (random between 10-30 fpm)
+        # add non-zero speeds
         for _ in range(min(active_period, num_rows - i)):
-            speed_data.append(random.uniform(10, 30))
+            speed_data.append(random.uniform(15, 30))
             i += 1
             if i >= num_rows:
                 break
@@ -43,7 +44,8 @@ def generate_excel_data():
         if speed == 0:
             od_data.append(current_od)
         else:
-            current_od = np.random.normal(mean_od, od_deviation)
+            new_od = np.random.normal(mean_od, od_deviation)
+            current_od = 0.95 * current_od + 0.05 * new_od
             od_data.append(current_od)
     
     df_od = pd.DataFrame({
@@ -57,7 +59,7 @@ def generate_excel_data():
     })
     
     # write to Excel file with two sheets
-    output_file = f'dummy_m{mean_od}_d{od_deviation}_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.xlsx'
+    output_file = f'dummy_r{sampling_rate}_m{mean_od}_d{od_deviation}_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.xlsx'
     with pd.ExcelWriter(output_file, engine='openpyxl') as writer:
         df_od.to_excel(writer, sheet_name='NDC_System_OD_Value', index=False)
         df_speed.to_excel(writer, sheet_name='YS_Pullout1_Act_Speed_fpm', index=False)
